2465c307dea958700ec05b4df16e49e8cbaa954d
Ellensohn <csap8008@uibk.ac.at>
2019-05-22 18:43:28 +0200

    steigung weg

--- adler/main.js
+++ adler/main.js
@@ -155,6 +155,38 @@ new L.GPX("AdlerwegEtappe01.gpx", {
     });
     controlElevation.addTo(karte);
     controlElevation.addData(e.line);
+    const gpxLinie = e.line.getLatLngs();
+    console.log(gpxLinie);
+    for (let i = 1; i < gpxLinie.length; i += 1) {
+        //console.log(gpxLinie[i]);
+        let p1 = gpxLinie[i-1];
+        let p2 = gpxLinie[i];
+        let dist = karte.distance(
+            [ p1.lat,p1.lng ],
+            [ p2.lat,p2.lng ]
+        );
+        let delta = (p2.meta.ele - p1.meta.ele);
+        let proz = (dist != 0 ? delta / dist * 100.0 : 0).toFixed(1);
+        console.log('Distanz:', dist, 'Höhendiff:', delta, 'Steigung: ', proz);
+        let farbe =
+        proz >= 10 ? '#d73027' :
+        proz >= 6 ? '#fc8d59' :
+        proz >= 2 ? '#fee08b' :
+        proz >= 0 ?  '#ffffbf' :
+        proz >= -6 ? '#d9ef8b' :
+        proz >= -10 ? '#91cf60' :
+        '#1a9850';
+        // ['#d73027','#fc8d59','#fee08b','#d9ef8b','#91cf60','#1a9850']
+        L.polyline(
+            [
+                [ p1.lat,p1.lng ],
+                [ p2.lat,p2.lng ],
+            ], {
+                color : farbe
+            }
+        ).addTo(karte);
+    }
 
 
-}).addTo(karte);
+
+});
